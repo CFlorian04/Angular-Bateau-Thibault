5854b086ee8e1ae380b8466cdb4329e6742f2c5e
CFlorian04 <113207681+CFlorian04@users.noreply.github.com>
2023-10-19 12:16:05 +0200

    Ajout Swipe back

--- Bateau-Thibault/src/app/home/home.page.ts
+++ Bateau-Thibault/src/app/home/home.page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { MainPageModel } from 'src/models/MainPage';
-import { CustomButton } from 'src/models/customButton';
 import { MainPageService } from '../main-page.service';
 import { Pages } from '../main-page.service';
+import { Gesture } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-home',
@@ -14,14 +16,17 @@ export class HomePage {
   MainPage! : MainPageModel;
 
   isEven! : boolean;
-  
+
+  private swipeCoord!: [number, number];
+  private swipeTime!: number;
 
   constructor(private mainPageS :MainPageService) {
   }
 
   ngOnInit() {
-    this.setMainPage(Pages.Accueil)
     console.log(this.mainPageS);
+
+    this.setMainPage(Pages.Accueil)
   }
 
   setMainPage(index :number) {
@@ -44,5 +49,27 @@ export class HomePage {
     return (index >= 0 && index <= 4)
   }
 
+  swipe(e: TouchEvent, when: string): void {
+    const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
+    const time = new Date().getTime();
+  
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+  
+      if (duration < 1000 && direction[0] > 30 && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) { 
+          //const swipe = direction[0] < 0 ? 'next' : 'previous';
+          // Do whatever you want with swipe
+
+          console.log(direction[0] < 0 ? 'next' : 'previous');
+
+          this.setMainPage(this.MainPage.index/10 >= 0 ? Math.trunc(this.MainPage.index/10) : 0)
+
+      }
+    }
+  }
 
 }
