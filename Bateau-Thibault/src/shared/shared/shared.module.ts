4396499137698da6f986becb5ae9c3ac4a523b1c
CalvinMP <91535810+CalvinMP@users.noreply.github.com>
2023-10-19 12:16:40 +0200

    small updates

--- Bateau-Thibault/src/shared/shared/shared.module.ts
+++ Bateau-Thibault/src/shared/shared/shared.module.ts
@@ -3,19 +3,18 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
-import { BackgroundComponent } from 'src/components/background/background.component';
 import { LinksComponent } from 'src/components/links/links.component';
 import { PoesieComponent } from 'src/components/poesie/poesie.component';
 import { ToolbarComponent } from 'src/components/toolbar/toolbar.component';
 
 
 @NgModule({
-  declarations: [BackgroundComponent, LinksComponent, PoesieComponent, ToolbarComponent],
+  declarations: [LinksComponent, PoesieComponent, ToolbarComponent],
   imports: [
     CommonModule, 
     IonicModule,
     RouterModule
   ],
-  exports: [BackgroundComponent, LinksComponent, PoesieComponent, ToolbarComponent]
+  exports: [LinksComponent, PoesieComponent, ToolbarComponent]
 })
 export class SharedModule { }
