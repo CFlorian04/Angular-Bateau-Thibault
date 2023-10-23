8a1207bbb1323f7ccddc0afeae3264d4fce5c736
Benjamin-Fiche <benjaminfiche@gmail.com>
2023-10-23 06:23:11 +0200

    Final commit

--- Bateau-Thibault/src/app/home/home.module.ts
+++ Bateau-Thibault/src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
@@ -9,6 +9,10 @@ import { MenuButtonComponent } from '../menu-button/menu-button.component';
 import { ContactComponent } from '../contact/contact.component';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { ShopComponent } from '../shop/shop.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   imports: [
@@ -17,6 +21,7 @@ import { ShopComponent } from '../shop/shop.component';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage, MenuButtonComponent, ContactComponent, ToolbarComponent, ShopComponent]
+  declarations: [HomePage, MenuButtonComponent, ContactComponent, ToolbarComponent, ShopComponent],
+  providers: [{provide: LOCALE_ID, useValue: 'fr' }]
 })
 export class HomePageModule {}
--- Bateau-Thibault/src/app/shop/shop.component.ts
+++ Bateau-Thibault/src/app/shop/shop.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { map } from 'rxjs';
+import { last, map } from 'rxjs';
 import { ShopInfo } from 'src/models/ShopInfo';
 import { CustomButton } from 'src/models/customButton';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,9 +11,158 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 })
 
 export class ShopComponent  implements OnInit {
-
   isAlertOpen : Boolean = false;
-  public alertButtons = ['OK'];
+  date : string = new Date().toISOString();
+  minDate: any = new Date().toISOString();
+  lastClicked : string = '';
+  
+  restaurantList : string[] = [
+    'Bistrot des Gascons',
+    'Les Fous de l\'Ile',
+    'Bistrot Landais',
+    'Villa 9-Trois',
+    'Bistrot du Sommelier'
+  ]
+  public alertButtons = [
+    {
+      text: 'Non',
+      role: 'cancel',
+      handler: () => {
+        console.log('Achat annulé');
+      },
+    },
+    {
+      text: 'Oui',
+      role: 'confirm',
+      handler: () => {
+        console.log('Achat confirmé');
+      },
+    },
+  ];
+
+  public actionSheetButtons = [
+    {
+      text: '0',       
+      handler: () => {
+        this.realPanierList = this.realPanierList.filter(item => item.name != this.lastClicked);
+        //this.realPanierList.find(item => item.name == this.lastClicked)!.num = 0;
+        /*this.panierList = this.panierList.reverse();
+        while (this.panierList.findIndex((item) => item.name == this.lastClicked) != -1)
+        {
+          this.panierList = this.panierList.slice(this.panierList.findIndex((item) => item.name == this.lastClicked), 1);
+          console.log(this.panierList);
+          console.log(this.lastClicked);
+        }
+        this.panierList = this.panierList.reverse();
+        this.setRealPanier();*/
+      }  
+    },
+    {
+      text: '1',     
+      handler: () => {
+        this.realPanierList.find(item => item.name == this.lastClicked)!.num = 1;
+        /*this.panierList = this.panierList.reverse();
+        let index : number = 0;
+        for (const elem of this.panierList)
+        {
+          if (elem.name == this.lastClicked)
+            index++;
+        }
+        if (index < 1)
+        {
+          while (index < 1)
+          {
+            //this.panierList.slice(this.panierList.findIndex((item) => item.name == this.lastClicked));
+            let item : CustomButton;
+            item = this.panierList.find((element) => element.name == this.lastClicked)!;
+            this.panierList.push({ name: item.name,
+              index: item.index,
+              image: item.image});
+            index++;
+          }
+        }
+        else if (index > 1)
+        {
+          while (index > 1)
+          {
+            this.panierList = this.panierList.slice(this.panierList.findIndex((item) => item.name == this.lastClicked));
+            index--;
+          }
+        }
+        this.panierList = this.panierList.reverse();
+        this.setRealPanier();*/
+      }  
+    },
+    {
+      text: '2',     
+      handler: () => {
+        this.realPanierList.find(item => item.name == this.lastClicked)!.num = 2;
+        /*this.panierList = this.panierList.reverse();
+        let index : number = 0;
+        for (const elem of this.panierList)
+        {
+          if (elem.name == this.lastClicked)
+            index++;
+        }
+        if (index < 2)
+        {
+          while (index < 2)
+          {
+            //this.panierList.slice(this.panierList.findIndex((item) => item.name == this.lastClicked));
+            let item : CustomButton;
+            item = this.panierList.find((element) => element.name == this.lastClicked)!;
+            this.panierList.push({ name: item.name,
+              index: item.index,
+              image: item.image});
+            index++;
+          }
+        }
+        else if (index > 2)
+        {
+          while (index > 2)
+          {
+            this.panierList = this.panierList.slice(this.panierList.findIndex((item) => item.name == this.lastClicked));
+            index--;
+          }
+        }
+        this.panierList = this.panierList.reverse();
+        this.setRealPanier();*/
+      }  
+    },
+    {
+      text: '3',     
+      handler: () => { 
+        this.realPanierList.find(item => item.name == this.lastClicked)!.num = 3;
+      }  
+    },
+    {
+      text: '4',     
+      handler: () => { 
+        this.realPanierList.find(item => item.name == this.lastClicked)!.num = 4;
+      }  
+    },
+    {
+      text: '5',     
+      handler: () => { 
+        this.realPanierList.find(item => item.name == this.lastClicked)!.num = 5;
+      }  
+    },
+    {
+      text: 'Ajouter 1',     
+      handler: () => { 
+        this.realPanierList.find(item => item.name == this.lastClicked)!.num++;
+      }  
+    },
+    {
+      text: 'Enlever 1',     
+      handler: () => { 
+        if (this.realPanierList.find(item => item.name == this.lastClicked)!.num == 1)
+          this.realPanierList = this.realPanierList.filter(item => item.name != this.lastClicked);
+        else
+          this.realPanierList.find(item => item.name == this.lastClicked)!.num--;
+      }  
+    }
+  ];
 
   @Input() panierList!: CustomButton[];
 
@@ -22,14 +171,13 @@ export class ShopComponent  implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.setRealPanier();
+  }
+
+  setRealPanier() {
+    this.realPanierList = [];
     for (const element of this.panierList) {
       console.log(element);
-      if (this.realPanierList.length == 0) {
-        this.realPanierList.push({ name: element.name, 
-          num: 1,
-          price: element.index,
-          image: element.image});
-      }
 
       for (let j = 0; j < this.realPanierList.length; j++)
       {
@@ -37,13 +185,21 @@ export class ShopComponent  implements OnInit {
         {
           this.realPanierList[j].num++;
         }
-        else if ((j+1) == this.realPanierList.length)
+        else if ((j + 1) == this.realPanierList.length)
         {
           this.realPanierList.push({ name: element.name, 
             num: 1,
             price: element.index,
             image: element.image});
+          break;
         }
+      }
+
+      if (this.realPanierList.length == 0) {
+          this.realPanierList.push({ name: element.name, 
+          num: 1,
+          price: element.index,
+          image: element.image});
       }
     }
     console.log(this.realPanierList);
@@ -61,5 +217,9 @@ export class ShopComponent  implements OnInit {
 
   setOpen(state : Boolean) {
     this.isAlertOpen = state;
+  }
+
+  onClick(item: string) {
+    this.lastClicked = item;
   }
 }
